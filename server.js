6106b76373dfbe39ee3a8f43885678558afcf6ea
smallplayz <900raiyan@gmail.com>
2022-11-09 14:19:13 -0500

    idek

--- server.js
+++ server.js
@@ -3,16 +3,35 @@ const { Socket } = require('socket.io')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, { cors: { orgin: '*'}})
-
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.post('/', (req, res, next) => {
+
+    const username = req.body.username
+    const password = req.body.username
+    res.cookie('username', username, { maxAge: 90 * 24 * 60 * 60 * 1000 });
+    res.render('index')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
+app.get('/logout', (req, res) => {
+    res.cookie('username', '', { maxAge: 0 });
+    res.redirect('login')
+})
+    
 server.listen(3000, () => {
     console.log('Server is running...')
 })  
